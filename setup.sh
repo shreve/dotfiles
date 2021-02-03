9d80af417ab084e2f8e24ea971e04a82ab631c8b
Jacob Evan Shreve <jacob@shreve.io>
2021-02-02 22:20:55 -0500

    Add list of packages to setup

--- setup.sh
+++ setup.sh
@@ -26,37 +26,33 @@ link() {
 
 link bin ~/bin
 
-link dunstrc ~/.config/dunst/dunstrc
+link irbrc.rb   ~/.irbrc
+link gemrc      ~/.gemrc
+link psqlrc.sql ~/.psqlrc
+link sqlite.sql ~/.sqliterc
+link xinitrc    ~/.xinitrc
+link xmodmap    ~/.Xmodmap
 
-link vim ~/.config/nvim
+link vim               ~/.config/nvim
+link config.fish       ~/.config/fish/config.fish
+link dunstrc           ~/.config/dunst/dunstrc
+link ranger.conf       ~/.config/ranger/rc.conf
+link konsole/konsolerc ~/.config/konsolerc
+link i3/conf           ~/.config/i3/config
+link i3/blocks.conf    ~/.config/i3blocks/config
 
-link config.fish ~/.config/fish/config.fish
-
-link gemrc ~/.gemrc
-
-link git/config ~/.gitconfig
-link git/ignore ~/.gitignore
-link git/message ~/.gitmessage
+link git/config   ~/.gitconfig
+link git/ignore   ~/.gitignore
+link git/message  ~/.gitmessage
 link git/template ~/.gittemplate
 
-link irbrc.rb ~/.irbrc
-
-link i3/conf ~/.config/i3/config
-link i3/blocks.conf ~/.config/i3blocks/config
-link i3/status ~/.i3status.conf
-
-link psqlrc.sql ~/.psqlrc
-
-link xinitrc ~/.xinitrc
-link xmodmap ~/.Xmodmap
-
 link konsole/ ~/.local/share/konsole
-link konsole/konsolerc ~/.config/konsolerc
 
 # link bin/keylight /etc/pm/sleep.d/10keylight
 
-link /home/jacob/.local/share/Trash/files ~/.trash
+# link services/syncthing.service /etc/systemd/system/syncthing.service
 
-
-link services/syncthing.service /etc/systemd/system/syncthing.service
-
+if which yay >/dev/null 2>&1; then
+  echo "Installing arch packages."
+  cat arch-packages.txt | xargs yay -Sy --noconfirm --needed
+fi
